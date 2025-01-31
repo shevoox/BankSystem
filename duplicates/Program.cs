5668ec0d56a241286bc24e1c5ed2f1b16bcdb59e
shevoox <catspeed225@gmail.com>
2025-01-31 23:25:05 +0200

    Update

---
+++ duplicates/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace duplicates
+{
+    internal class Program
+    {
+        static void CheckDublicates(string[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)//1 2 3 4
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        throw new Exception($"Duplicate number found: {numbers[i]}");
+
+                    }
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Enter numbers separated by spaces: ");
+                string[] StringArr = Console.ReadLine().Split(' ');
+                CheckDublicates(StringArr);
+                Console.WriteLine("No duplicates found!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+    }
+}
